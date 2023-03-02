f307571adc9205a223c3706da81185aa4d1503a8
“vivek191290” <vivekpatil9525@gmail.com>
2023-03-02 16:09:09 +0530

    Add: day6 practice problem added

---
+++ day6/multiplication.sh
@@ -0,0 +1,5 @@
+read -p "Enter first number:" number1
+read -p "Enter first number:" number2
+multiplication=$(($number1 * $number2))
+echo "Multiplication of two number is:" $multiplication
+
---
+++ day6/whileLoopprogram.sh
@@ -0,0 +1,6 @@
+i=0
+while [ 256 -gt $i ]
+do
+((i++))
+echo $i
+done
